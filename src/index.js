6e875718236b197fbf5dd47ade0f73de0e319d72
Horacio Correia <rmcorre@gmail.com>
2021-10-28 16:30:27 +0000

    Reset browser defaults.

--- src/index.js
+++ src/index.js
@@ -4,8 +4,9 @@ import App from './App';
 
 import '@fontsource/bitter/400.css';
 import '@fontsource/bitter/variable.css';
-
 import '@fontsource/montserrat/400.css';
+
+import 'normalize.css';
 
 // eslint-disable-next-line react/jsx-filename-extension
 ReactDOM.render(<App />, document.getElementById('root'));
