98bde862b94e846c65e600cf1c37ec0a03183a57
Horacio Correia <rmcorre@gmail.com>
2021-10-28 16:16:07 +0000

    Install fonts.

--- src/index.js
+++ src/index.js
@@ -2,5 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+import '@fontsource/bitter/400.css';
+import '@fontsource/bitter/variable.css';
+
+import '@fontsource/montserrat/400.css';
+
 // eslint-disable-next-line react/jsx-filename-extension
 ReactDOM.render(<App />, document.getElementById('root'));
