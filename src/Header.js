d672b5b42831ac41f387a60907aa2c41f4f5d3b3
Horacio Correia <rmcorre@gmail.com>
2021-10-30 08:35:01 +0000

    Add header mark-up and styles.

--- src/Header.js
+++ src/Header.js
@@ -1,7 +1,23 @@
 import React from 'react';
 
-function Header() {
-  return <header>Header Placeholder</header>;
-}
+import styles from './Header.module.css';
+import logo from './img/logo.svg';
+
+const Header = () => (
+  <header className={styles.header}>
+    <nav className={styles.navbar}>
+      <div className={styles.brand}>
+        <a href="/">
+          <img src={logo} alt="Reddit Timer logo" width="150" height="36" />
+        </a>
+      </div>
+      <ul className={styles.nav}>
+        <li>Search</li>
+        <li>How it works</li>
+        <li>About</li>
+      </ul>
+    </nav>
+  </header>
+);
 
 export default Header;
