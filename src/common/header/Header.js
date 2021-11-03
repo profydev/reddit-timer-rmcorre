0c7c7a16f70f001a6e33328896f5ebbc1eec60f5
Horacio Correia <rmcorre@gmail.com>
2021-11-03 01:02:47 -0100

    Modify file structure

--- src/common/header/Header.js
+++ src/common/header/Header.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 import styles from './Header.module.css';
-import logo from './img/logo.svg';
+import logo from './logo.svg';
 
 const Header = () => (
   <header className={styles.header}>
