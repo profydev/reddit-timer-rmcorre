1cb5e5ed48f0988eb571e8f494f74b04421cc458
Horacio Correia <rmcorre@gmail.com>
2021-11-05 01:48:07 -0100

    Define theme styles.

---
+++ src/style/theme.js
@@ -0,0 +1,27 @@
+const theme = {
+  color: {
+    text: '#93918f',
+    black: '#000000',
+    white: '#ffffff',
+    gray300: '#d5d5d5',
+    gray700: '#636363',
+  },
+  fontFamily: {
+    default: '"Montserrat", sans-serif',
+    headline: '"Bitter", serif',
+  },
+  fontSize: {
+    default: '1rem',
+  },
+  lineHeight: {
+    default: 1.69,
+  },
+  margin: {
+    default: 'auto',
+  },
+  width: {
+    default: '1440px',
+  },
+};
+
+export default theme;
