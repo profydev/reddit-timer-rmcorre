5b767677cf058e725545d79f03d302ad6b497773
Horacio Correia <rmcorre@gmail.com>
2021-11-14 00:17:44 -0100

    Chore refactor css (#9)

--- src/style/GlobalStyle.js
+++ src/style/GlobalStyle.js
@@ -6,12 +6,23 @@ const GlobalStyle = createGlobalStyle`
   }
 
   body {
+    color: ${(props) => props.theme.color.default};
     font-family:  ${(props) => props.theme.fontFamily.default};
     font-size: ${(props) => props.theme.fontSize.default};
     line-height: ${(props) => props.theme.lineHeight.default};
-    color: ${(props) => props.theme.color.text};
     max-width: 1440px;
-    margin: auto;
+  }
+
+  h1, h2, h3, h4, h5, h6 {
+    color: ${(props) => props.theme.color.black};
+    font-family: ${(props) => props.theme.fontFamily.heading};
+    font-weight: normal;
+    margin: 0;
+  }
+
+  h1 {
+    font-size: ${(props) => props.theme.fontSize.h1};
+    line-height: ${(props) => props.theme.lineHeight.h1};
   }
 `;
 
