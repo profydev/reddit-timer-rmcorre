ee3e5a0aac5c1e0a84b5e0e9a90be57f67f55ea1
Horacio Correia <rmcorre@gmail.com>
2021-11-24 04:33:06 -0100

    Refactor Form

--- src/page-search/Form.js
+++ src/page-search/Form.js
@@ -1,34 +1,34 @@
 import React, { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
+import { useParams, useHistory } from 'react-router-dom';
 import * as S from './Form.style';
 
 const Form = () => {
-  const [value, setValue] = useState('');
-  const { subreddit } = useParams();
-
-  useEffect(() => {
-    setValue(subreddit);
-  }, [setValue, subreddit]);
+  const { subreddit: initialSubbreddit } = useParams();
+  const [subreddit, setSubreddit] = useState(initialSubbreddit);
+  const history = useHistory();
 
   const handleOnChange = (e) => {
-    setValue(e.target.value);
+    setSubreddit(e.target.value);
   };
 
-  const submitHandler = (e) => {
+  const handleSubmit = (e) => {
     e.preventDefault();
-    const url = `http://localhost:3000/search/${value}`;
-    window.location.href = url;
+    history.push(`/search/${subreddit}`);
   };
+
+  useEffect(() => {
+    setSubreddit(initialSubbreddit);
+  }, [initialSubbreddit]);
 
   return (
-    <S.Form onSubmit={submitHandler}>
+    <S.Form onSubmit={handleSubmit}>
       <S.Label htmlFor="subreddit">r /</S.Label>
       <S.Input
         id="subreddit"
         type="text"
         name="subreddit"
         onChange={handleOnChange}
-        value={value}
+        value={subreddit}
       />
       <S.Button>SEARCH</S.Button>
     </S.Form>
