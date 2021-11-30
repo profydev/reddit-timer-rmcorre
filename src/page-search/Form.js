370be2e151b112945cb8f509eabd080b0ccdc019
Horacio Correia <rmcorre@gmail.com>
2021-11-30 02:37:03 -0100

    Lift state to SearchPage

--- src/page-search/Form.js
+++ src/page-search/Form.js
@@ -1,38 +1,41 @@
-import React, { useEffect, useState } from 'react';
-import { useParams, useHistory } from 'react-router-dom';
+/* eslint-disable react/prop-types */
+/* eslint-disable react/destructuring-assignment */
+import React from 'react';
+// import { useParams, useHistory } from 'react-router-dom';
 import * as S from './Form.style';
 
-const Form = () => {
-  const { subreddit: initialSubbreddit } = useParams();
-  const [subreddit, setSubreddit] = useState(initialSubbreddit);
-  const history = useHistory();
+const Form = (props) => (
+  // const { subreddit: initialSubbreddit } = useParams();
+  // const [subreddit, setSubreddit] = useState(initialSubbreddit);
+  // const history = useHistory();
 
-  const handleOnChange = (e) => {
-    setSubreddit(e.target.value);
-  };
+  // const handleOnChange = (e) => {
+  //   setSubreddit(e.target.value);
+  // };
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    history.push(`/search/${subreddit}`);
-  };
+  // const handleSubmit = (e) => {
+  //   e.preventDefault();
+  //   history.push(`/search/${subreddit}`);
+  // };
 
-  useEffect(() => {
-    setSubreddit(initialSubbreddit);
-  }, [initialSubbreddit]);
+  // useEffect(() => {
+  //   setSubreddit(initialSubbreddit);
+  // }, [initialSubbreddit]);
 
-  return (
-    <S.Form onSubmit={handleSubmit}>
+  <S.Section>
+    <S.Heading>Find the best time for a subreddit</S.Heading>
+    <S.Form onSubmit={props.handleSubmit}>
       <S.Label htmlFor="subreddit">r /</S.Label>
       <S.Input
         id="subreddit"
         type="text"
         name="subreddit"
-        onChange={handleOnChange}
-        value={subreddit}
+        onChange={props.handleOnChange}
+        value={props.subreddit}
       />
       <S.Button>SEARCH</S.Button>
     </S.Form>
-  );
-};
+  </S.Section>
+);
 
 export default Form;
