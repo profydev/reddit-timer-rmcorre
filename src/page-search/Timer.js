4ba72ea1aa6bb4613e521bdb6f48d3fc4a6fd016
Horacio Correia <rmcorre@gmail.com>
2021-11-30 02:40:32 -0100

    Add Timer

---
+++ src/page-search/Timer.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import * as S from './Timer.style';
+
+// eslint-disable-next-line react/prop-types
+const Timer = ({ children }) => <S.Section>{children}</S.Section>;
+
+export default Timer;
---
+++ src/page-search/Timer.style.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+// eslint-disable-next-line import/prefer-default-export
+export const Section = styled.section`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin: 0 auto;
+`;
